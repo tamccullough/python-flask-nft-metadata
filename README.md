3f6097b8c791fda449b675dfef027a045e9f8814
Todd McCullough <howdytodd@gmail.com>
2021-05-31 09:53:26 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+## This is a Remix of the following
+## Python Metadata API for OpenSea Creatures
+found here [OpenSea Metadata API Python](https://github.com/ProjectOpenSea/metadata-api-python)
+
+### About
+It is still a;
+Very simple sample Python Flask app for serving the ERC721 metadata for a collection of items.
+
+## Requirements
+
+### Python 3
+
+You'll need a machine with Python 3 installed.
+
+### The Google Cloud Storage has been stripped
+
+I have served this through Heroku.
+
+## Setup
+
+Create a virtualenv with Python3
+run `pip install --upgrade pip`
+run `pip install -r requirements.txt`.
+
+## Running and Testing
+
+Run the API with `python app.py` and hit http://localhost:5000/0
+
+## Deploying
+
+To deploy on Heroku;
+use Heroku CLI
+heroku login -i
+cd <to this main directory>
+heroku create
+git init
+git add .
+git commit -am 'comments go here'
+git push heroku master
+
+then hit http://appname.herokuapp.com/0
---
+++ app.py
@@ -0,0 +1,63 @@
+# https://github.com/tamccullough - questions? twitter @tamccullough
+from flask import Flask,jsonify,request
+import pandas as pd
+
+# for this file to not be so long, I have placed a dictionary in the following py file
+# to edit there. Saves some clutter in this area
+import collection_func as cf
+
+# edit this file however you see fit. If it suits you to make changes to it for your work. Do so.
+
+# set the name for the collection
+collection_name = 'DeeManz'
+
+app = Flask(__name__)
+
+# serve the json file metadata required for the token
+@app.route('/<token_id>') # url includes the token_id number
+def item(token_id):
+    token_id = int(token_id)
+    # data for the collection of images is stored in a csv file, loaded into a dataframe
+    df = pd.read_csv(f'collection/collection_data.csv')
+    # get the relevant slice from the dataframe, selecting from the token_id
+    item = df.loc[token_id]
+    # get the url root which will be printed for the nft reference
+    url_path = request.url_root
+    #external_url = 'https://testnets.opensea.io/assets/<asset_contract_address>/{token_id}'
+
+    # taking the
+    attributes = []
+    for i in item.keys():
+        if i in ['name','image']: # pass any column you have created that should not be an attribute
+            pass
+        else:
+            attribute = {
+                'trait_type':i,
+                'value':cf.get_collection_item(i,item[i]) # using the dictionary found in collection_func get the appropriate strings
+            }
+            attributes.append(attribute)
+
+    # using Flask jsonify to pass the selected data as json file format
+    return jsonify({
+        'name': item['name'],
+        'external_url': f'{url_path}{token_id}',
+        'image': f'{url_path}image/{token_id}',
+        'description': f'{collection_name} that enjoys spreading the power of the Dark Lord.',
+        'attributes': attributes,
+    })
+
+# serve the selected image, important for the image to show up within the nft
+@app.route('/image/<token_id>')
+def image(token_id):
+    token_id = int(token_id)
+    df = pd.read_csv(f'collection/collection_data.csv')
+    item = df.loc[token_id]
+    # if you haven't named your images using a lower case version of your collection name
+    # you need to change the following string to point to the images
+    image_url = f'collection/{collection_name.lower()}{token_id+1}.png'
+
+    return send_file(image_url, mimetype='image/png')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
